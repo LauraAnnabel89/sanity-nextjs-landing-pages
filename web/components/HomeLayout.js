e7d49eae0855a2cd6e2005e84f208e83629ae71c
Laura-Annabel Tombs <home@Laura-Annabels-MacBook-Pro.local>
2020-05-29 12:41:32 +0100

    Adjusted routes to have a homepage as custom page and the rest as landing pages

--- web/components/HomeLayout.js
+++ web/components/HomeLayout.js
@@ -1,33 +1,41 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Head from 'next/head'
-import styles from './Homelayout.module.css'
+import React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
 
-import {LogoJsonLd} from 'next-seo'
-import Header from './Header'
+import { LogoJsonLd } from "next-seo";
+import Header from "./Header";
+import Footer from "./Footer";
 
-function HomeLayout (props) {
-  const {config, children} = props
+function HomeLayout(props) {
+  const { config, children } = props;
 
   if (!config) {
-    console.error('Missing config')
-    return <div>Missing config</div>
+    console.error("Missing config");
+    return <div>Missing config</div>;
   }
 
-  const {title, mainNavigation, footerNavigation, footerText, logo, url} = config
-  const logoUrl = logo && logo.asset && logo.asset.url
+  const {
+    title,
+    mainNavigation,
+    footerNavigation,
+    footerText,
+    logo,
+    url,
+    socialmedialogos,
+  } = config;
+  const logoUrl = logo && logo.asset && logo.asset.url;
 
   return (
     <>
       <Head>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width, viewport-fit=cover' />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width, viewport-fit=cover" />
       </Head>
-      <div className={styles.homeContainer}>
+      <div className="container">
         <Header title={title} navItems={mainNavigation} logo={logo} />
-        <div className='content'>{children}</div>
+        <div className="content">{children}</div>
       </div>
     </>
-  )
+  );
 }
 
 HomeLayout.propTypes = {
@@ -35,8 +43,15 @@ HomeLayout.propTypes = {
   config: PropTypes.shape({
     title: PropTypes.string,
     mainNavigation: PropTypes.arrayOf(PropTypes.object),
-    url: PropTypes.string
-  })
-}
+    footerNavigation: PropTypes.arrayOf(PropTypes.object),
+    footerText: PropTypes.arrayOf(PropTypes.object),
+    logo: PropTypes.shape({
+      asset: PropTypes.shape({
+        url: PropTypes.string,
+      }),
+    }),
+    url: PropTypes.string,
+  }),
+};
 
-export default HomeLayout
+export default HomeLayout;
