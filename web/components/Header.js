7bfe76effc5349234442eca9686379e199d71ac9
Luigi Mannoni <hello@luigimannoni.com>
2020-07-13 19:13:01 +0100

    Fixes on navbar

--- web/components/Header.js
+++ web/components/Header.js
@@ -1,91 +1,91 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import Link from "next/link";
-import { withRouter } from "next/router";
-import SVG from "react-inlinesvg";
-import styles from "./Header.module.css";
-import HamburgerIcon from "./icons/Hamburger";
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import Link from 'next/link'
+import {withRouter} from 'next/router'
+import SVG from 'react-inlinesvg'
+import styles from './Header.module.css'
+import HamburgerIcon from './icons/Hamburger'
 
 class Header extends Component {
-  state = { showNav: false };
+  state = {showNav: false};
 
   static propTypes = {
     router: PropTypes.shape({
       pathname: PropTypes.string,
       query: PropTypes.shape({
-        slug: PropTypes.string,
+        slug: PropTypes.string
       }),
-      events: PropTypes.any,
+      events: PropTypes.any
     }),
     title: PropTypes.string,
     navItems: PropTypes.arrayOf(
       PropTypes.shape({
         title: PropTypes.string.isRequired,
         slug: PropTypes.shape({
-          current: PropTypes.string,
-        }).isRequired,
+          current: PropTypes.string
+        }).isRequired
       })
     ),
     logo: PropTypes.shape({
       asset: PropTypes.shape({
-        url: PropTypes.string,
+        url: PropTypes.string
       }),
-      logo: PropTypes.string,
-    }),
+      logo: PropTypes.string
+    })
   };
 
-  componentDidMount() {
-    const { router } = this.props;
-    router.events.on("routeChangeComplete", this.hideMenu);
+  componentDidMount () {
+    const {router} = this.props
+    router.events.on('routeChangeComplete', this.hideMenu)
   }
 
-  componentWillUnmount() {
-    const { router } = this.props;
-    router.events.off("routeChangeComplete", this.hideMenu);
+  componentWillUnmount () {
+    const {router} = this.props
+    router.events.off('routeChangeComplete', this.hideMenu)
   }
 
   hideMenu = () => {
-    this.setState({ showNav: false });
+    this.setState({showNav: false})
   };
 
   handleMenuToggle = () => {
-    const { showNav } = this.state;
+    const {showNav} = this.state
     this.setState({
-      showNav: !showNav,
-    });
+      showNav: !showNav
+    })
   };
 
   renderLogo = (logo) => {
     if (!logo || !logo.asset) {
-      return null;
+      return null
     }
 
-    if (logo.asset.extension === "svg") {
-      return <SVG src={logo.asset.url} className={styles.logo} />;
+    if (logo.asset.extension === 'svg') {
+      return <SVG src={logo.asset.url} className={styles.logo} />
     }
 
-    return <img src={logo.asset.url} alt={logo.title} className={styles.logo} />;
+    return <img src={logo.asset.url} alt={logo.title} className={styles.logo} />
   };
 
-  render() {
-    const { title = "Missing title", navItems, router, logo, reverseColour } = this.props;
-    const { showNav } = this.state;
+  render () {
+    const {title = 'Missing title', navItems, router, logo, reverseColour} = this.props
+    const {showNav} = this.state
 
     return (
       <div
         className={styles.root}
         data-show-nav={showNav}
-        style={{ color: reverseColour && "black" }}
+        style={{color: reverseColour && 'black'}}
       >
         <h1 className={styles.branding}>
           <Link
             href={{
-              pathname: "/",
+              pathname: '/',
               query: {
-                slug: "/",
-              },
+                slug: '/'
+              }
             }}
-            as="/"
+            as='/'
           >
             <a title={title}>DAWN</a>
           </Link>
@@ -94,27 +94,27 @@ class Header extends Component {
           <ul className={styles.navItems}>
             {navItems &&
               navItems.map((item) => {
-                const { slug, title, _id } = item;
+                const {slug, title, _id} = item
                 const isActive =
-                  router.pathname === "/LandingPage" && router.query.slug === slug.current;
+                  router.pathname === '/LandingPage' && router.query.slug === slug.current
                 return (
                   <li key={_id} className={styles.navItem}>
                     <Link
                       href={{
-                        pathname: "/LandingPage",
-                        query: { slug: slug.current },
+                        pathname: '/LandingPage',
+                        query: {slug: slug.current}
                       }}
                       as={`/${slug.current}`}
                     >
                       <a
-                        data-is-active={isActive ? "true" : "false"}
-                        style={{ borderBottom: reverseColour && "none" }}
+                        data-is-active={isActive ? 'true' : 'false'}
+                        style={{borderBottom: reverseColour && 'none'}}
                       >
                         {title}
                       </a>
                     </Link>
                   </li>
-                );
+                )
               })}
           </ul>
           <button className={styles.showNavButton} onClick={this.handleMenuToggle}>
@@ -126,8 +126,8 @@ class Header extends Component {
           </button>
         </nav>
       </div>
-    );
+    )
   }
 }
 
-export default withRouter(Header);
+export default withRouter(Header)
