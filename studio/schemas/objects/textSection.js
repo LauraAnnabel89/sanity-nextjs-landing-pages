b134936e1fbb64032890f22bbc477cfe343448f7
Luigi Mannoni <hello@luigimannoni.com>
2020-07-23 20:12:45 +0100

    Save partial work on Contacts us page

--- studio/schemas/objects/textSection.js
+++ studio/schemas/objects/textSection.js
@@ -17,6 +17,30 @@ export default {
       name: 'text',
       type: 'portableText',
       title: 'Text'
+    },
+    {
+      title: 'Text alignment',
+      name: 'textAlign',
+      type: 'string',
+      options: {
+        list: [
+          {title: 'Center (Default)', value: ''},
+          {title: 'Left', value: 'left'},
+          {title: 'Right', value: 'right'}
+        ]
+      }
+    },
+    {
+      title: 'Font weight',
+      name: 'fontWeight',
+      type: 'string',
+      options: {
+        list: [
+          {title: 'Normal (Default)', value: ''},
+          {title: 'Force thin', value: 'thin'},
+          {title: 'Force bold', value: 'bold'}
+        ]
+      }
     }
   ],
   preview: {
