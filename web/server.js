03b6b3b111da74f543366e5c885f213792f98082
Luigi Mannoni <hello@luigimannoni.com>
2020-08-21 15:41:35 +0100

    Push [slug] page and customize build

--- web/server.js
+++ web/server.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const next = require('next')
-const routes = require('next-routes')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({dev})
-
-const r = routes().add({name: 'LandingPage', pattern: '/:page', page: 'LandingPage'})
-const handler = r.getRequestHandler(app)
+const handle = app.getRequestHandler()
 
 const PROD_PORT = process.env.PORT || 80
 const PORT = dev ? 3000 : PROD_PORT
@@ -15,10 +12,13 @@ app.prepare()
   .then(() => {
     const server = express()
 
-    server.use(handler)
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
     server.listen(PORT, (err) => {
       if (err) throw err
-      console.log(`> Ready on http://localhost:${PORT}`)
+      console.log('> Ready on http://localhost:3000')
     })
   })
   .catch((ex) => {
